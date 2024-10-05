db1d10b66df1f0fdd23af9f288285e6d3e8bc2a0
Omesh Sachethana <sachethana12@gmail.com>
2024-10-05 18:06:19 +0530

    Updated RefrotfitInstance with BASE URL

---
+++ app/src/main/java/com/example/ecommerce/data/network/RetrofitInstance.kt
@@ -0,0 +1,20 @@
+package com.example.ecommerce.data.network
+
+import com.example.ecommerce.utils.Constants  // Import the Constants object
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "http://localhost:5207/api/" // Make sure this is correct
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val api: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+}
