deda430956f3c6fe4a314131054c4ee9c8a5be46
Menusha Katipearachchi <ekettipearachchi@gmail.com>
2024-10-07 21:49:28 +0530

    Added material icons extended library and modified profile page source

--- app/src/main/java/com/example/ecommerce/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/ecommerce/ui/profile/ProfileScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Logout
-import androidx.compose.material.icons.filled.Logout
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -14,12 +13,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.ecommerce.R
 
 @Composable
 fun ProfileScreen(
